1030118960398793c6ddf64f96441d660b23a7b3
Nagarei <Nagarei@users.noreply.github.com>
2021-08-16 15:44:35 +0900

    fix

--- sql/2_patch.sql
+++ sql/2_patch.sql
@@ -4,5 +4,5 @@
 ALTER TABLE `isu` ADD `image_hash` VARCHAR(32) NOT NULL DEFAULT "";
 UPDATE `isu` SET `image_hash`=MD5(`image`);
 
-UPDATE `isu_condition` SET `condition_level` = "i" WHERE condition="is_dirty=false,is_overweight=false,is_broken=false";
-UPDATE `isu_condition` SET `condition_level` = "c" WHERE condition="is_dirty=true,is_overweight=true,is_broken=true";
+UPDATE `isu_condition` SET `condition_level` = "i" WHERE `condition`="is_dirty=false,is_overweight=false,is_broken=false";
+UPDATE `isu_condition` SET `condition_level` = "c" WHERE `condition`="is_dirty=true,is_overweight=true,is_broken=true";
