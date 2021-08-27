ce4825059084d365b9471e8c97acd23de16bc6f1
Nagarei <Nagarei@users.noreply.github.com>
2021-08-27 16:17:13 +0900

    fix condition_level

--- sql/2_patch.sql
+++ sql/2_patch.sql
@@ -2,6 +2,9 @@
 
 
 
+ALTER TABLE `isu_condition` DROP id;
+ALTER TABLE `isu_condition` DROP created_at;
+ALTER TABLE `isu_condition` ADD `condition_level` VARCHAR(1) NOT NULL DEFAULT "w";
 UPDATE `isu_condition` SET `condition_level` = "i" WHERE `condition`="is_dirty=false,is_overweight=false,is_broken=false";
 UPDATE `isu_condition` SET `condition_level` = "c" WHERE `condition`="is_dirty=true,is_overweight=true,is_broken=true";
 
